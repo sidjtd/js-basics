a394565fb1289518bf65ffaf9a5e92b5871ce1b0
Sidney <user@SGD-MBP.local>
2016-06-06 18:07:19 -1000

    first day first work

--- basics.js
+++ basics.js
@@ -1,24 +1,44 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = 'Sid'; 
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+var person = {
+	name:myName,
+	age:89
+};
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+var canDrive = true;
+
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+function greet (name) {
+	( console.log, 'Hello, my name is ' + person.name );
+}
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+var dataTypes = [null,undefined,true,'cat',24,[],{}];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+var dog = {
+	name:'Spot',
+	bark: function(){console.log("Woof woof mother fucker")}
+};
